850a06f2de379a10aa6b1ed7f77d0bffc7ea5241
Boris Kolesnikov <barbariskolesnikov@gmail.com>
2025-07-06 01:31:02 +0300

    Add license notice to main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 """
-Reads a Gemini capsule (tested on smol.pub) and creates a .epub file
+Reads a Gemini capsule and creates a .epub file
+
+Copyright (C) 2025 Boris Kolesnikov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Based on following sources:
 https://github.com/aerkalov/ebooklib
 https://tildegit.org/solderpunk/gemini-demo-1
+https://github.com/eapl-gemugami/capsule-to-epub
 """
 
 # Python Built-in
